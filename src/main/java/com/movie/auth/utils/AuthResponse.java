06f37c7835aad0555df3116f5493adcf2338a7e4
bishal1998 <bishalpaudel1998@gmail.com>
2024-07-02 17:37:07 +0545

    AuthResponse created

---
+++ src/main/java/com/movie/auth/utils/AuthResponse.java
@@ -0,0 +1,16 @@
+package com.movie.auth.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class AuthResponse {
+    private String accessToken;
+
+    private String refreshToken;
+}
